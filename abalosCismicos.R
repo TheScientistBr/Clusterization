de4c908f16e00617115a4a64c784b08eac0c2add
dbranquinho <delermando@gmail.com>
2017-04-07 16:10:57 -0300

    bug fix

--- abalosCismicos.R
+++ abalosCismicos.R
@@ -13,19 +13,23 @@ summary(eData$mag)
 summary(eData$depth)
 
 tree <- hclustvar(eData[,c(2:5)],eData[,c(6,22)])
-stab <- stability(tree, graph = FALSE,B = 10)
+
+# Um procedimento bootstrap também é proposto para avaliar a estabilidade das partições.
+# Em K = 2, 3,. . . , P - 1 clusters e, em seguida, para ajudar o usuário a determinar 
+# um número adequado de grupos de variáveis.
+
+stab <- stability(tree, graph = FALSE,B = 100)
 
 nrCluster <- which.is.max(stab$meanCR)
 nrCluster
+plot(stab)
 
 set.seed(1234)
+clusterModel <- kmeans(iris[, 1:4], centers = 3, nstart = 100)
+clusterModel
 
 plot(eData$mag, eData$depth, col = clusterModel$cluster, pch = 19, xlab = "Magnitude", 
      ylab = "Depth", main = "Terremotos")
-par(new=TRUE)
-
-clusterModel <- kmeans(iris[, 1:4], centers = 3, nstart = 100)
-clusterModel
 
 eData[clusterModel$cluster == 1, ]
 
@@ -38,5 +42,5 @@ newComp <- cbind(pc.comp1, pc.comp2)
 cl <- kmeans(newComp, nrCluster)
 plot(pc.comp1, pc.comp2,col=clusterModel$cluster,xlab = "Componente 1", ylab = "Componente 2",
      main = "KMeans & PCA")
-points(cl$centers, pch=16)
+points(cl$centers, pch=19)
 
